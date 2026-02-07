c3851519bd3d32ec4025f633046e7e219faeee1b
lalithya-bc17 <lalithyabc@gmail.com>
2026-02-07 21:26:32 +0530

    added debug false to check

--- eduvillage/settings.py
+++ eduvillage/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "certificate-verification-backend-7gpb.onrender.com",

e244ccf4b7ea02c283b617160bd543e839ab4bf1
lalithya-bc17 <lalithyabc@gmail.com>
2026-02-11 13:08:08 +0530

    designing admin dashboard

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # ✅ JWT LOGIN WITH ROLE
     path("token/", views.CustomTokenView.as_view(), name="token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/admin/stats/", admin_stats),
-    path("api/admin/users/", admin_users),
+    path("admin/stats/", views.admin_stats),
+    path("admin/users/", views.admin_users),
     path("api/bootstrap-admin/", bootstrap_admin),
 ]
